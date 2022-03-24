d66d812d0935005853afaf6c06ca84c5da11cb44
Juliana Matos <jumatosk@gmail.com>
2022-03-24 16:59:22 -0300

    initial layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,58 @@
 <template>
-  <hello-world />
+  <v-fragment>
+    <v-app-bar clipped-right flat height="60" >
+      <v-responsive>
+        <v-row>
+          <v-col v-for="n in 4" :key="n">
+            <v-text-field
+              dense
+              flat
+              hide-details
+              rounded
+              solo-inverted
+              disabled
+            />
+          </v-col>
+        </v-row>
+      </v-responsive>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" width="300">
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        color="grey lighten-3"
+        mini-variant
+      >
+        <v-avatar
+          class="d-block text-center mx-auto mt-4"
+          color="grey darken-1"
+          size="36"
+        ></v-avatar>
+
+        <v-divider class="mx-3 my-5"></v-divider>
+
+        <v-avatar
+          v-for="n in 6"
+          :key="n"
+          class="d-block text-center mx-auto mb-9"
+          color="grey lighten-1"
+          size="28"
+        ></v-avatar>
+      </v-navigation-drawer>
+
+      <v-list class="pl-14" shaped>
+        <v-list-item v-for="n in 8" :key="n" link>
+          <v-list-item-content>
+            <v-list-item-title>Item {{ n }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-fragment>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
 export default {
-  components: {
-    HelloWorld,
-  },
 };
 </script>
