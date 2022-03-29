60df2383a987fb3e9c3711f66af750e89e6f4fb2
Juliana Matos <jumatosk@gmail.com>
2022-03-29 16:57:39 -0300

    ajustes

--- src/components/Template/AppBar.vue
+++ src/components/Template/AppBar.vue
@@ -8,13 +8,31 @@
       <div style="font-size: 17px">CAMAÇARI</div></v-toolbar-title
     >
     <v-spacer></v-spacer>
-    <v-icon class="mr-10" color="white" size="20" style="margin-left: -7px"
-      >mdi-cog-outline
-    </v-icon>
+
+    <v-menu bottom left>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          class="mr-10"
+          size="18"
+          icon
+          color="white"
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon>mdi-cog-outline</v-icon>
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item v-for="(item, i) in items" :key="i">
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <div class="text-color-white">Paulo Campos</div>
     <v-avatar class="d-flex text-right mx-auto" size="42">
       <v-icon size="35"> mdi-account-circle </v-icon>
     </v-avatar>
-    <div class="text-color-white">Paulo Campos</div>
     <div class="d-flex align-center"></div>
   </v-app-bar>
 </template>
@@ -22,7 +40,9 @@
 <script>
 export default {
   data: () => {
-    return {};
+    return {
+      items: [{ title: "Perfil" }, { title: "Trocar Senha" }],
+    };
   },
 };
 </script>
